f694045242d718975e08422f27d6a93f50e10935
ManonVentura <m.ventura@vatel.fr>
2022-11-24 12:39:34 +0100

    ajouts emoji et display post

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 
 import './App.css';
 import useMarkdown from './hooks/useMarkdown';
+import usePosts from './hooks/usePosts';
 
 function App() {
-
   const {bind, result} = useMarkdown()
+  const {post, handlePost} = usePosts()
+
+
+  let falseData = []
+  console.log('result',result)
+  console.log('post', post)
+  const posts = post.map((data, i) => {
+    return (
+      <div className='post' key= {i} dangerouslySetInnerHTML={{__html: data}}> 
+      </div>
+  )
+  }
+  )
+
   return (
     <div className="App">
+      <div className='header'>
+        <h1>My blog</h1>
+      </div>
+      <div className="writeContainer">
       <textarea className="writeText" placeholder="write here" {...bind}/>
       <div className="showText" dangerouslySetInnerHTML={{__html: result}}/>
+      <button onClick={() => handlePost(result)} className="postBtn">Post</button>
+      </div>
+      <div className="displayPostsContainer">
+        {posts}
+      </div>
     </div>
   );
 }
--- src/hooks/useMarkdown.js
+++ src/hooks/useMarkdown.js
@@ -1,16 +1,20 @@
 import {useState} from "react";
 import * as MarkdownIt from "markdown-it"
+var emoji = require('markdown-it-emoji')
 
 
 export default function useMarkdown() {
     const [text, setText] = useState("")
     // Markdown --> modifier mise en page texte via des commandes style **texte en gras** ou # titre
     var md = new MarkdownIt();
-    var result = md.render(text);
+    md.use(emoji)
+    var result = md.render(text, emoji);
 
-    const handleChange= e => 
-    setText(e.target.value);
-
+    const handleChange= (e) => {
+        setText(e.target.value);
+    }
+    
+    
     return {
         result,
         //
